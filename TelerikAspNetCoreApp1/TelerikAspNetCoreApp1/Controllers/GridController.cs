ecf7b36d41b25fdda773b9de61dd05225bd632f4
david <shinvkvk@gmail.com>
2018-02-19 18:43:09 +0900

    nodejs 추가

--- TelerikAspNetCoreApp1/TelerikAspNetCoreApp1/Controllers/GridController.cs
+++ TelerikAspNetCoreApp1/TelerikAspNetCoreApp1/Controllers/GridController.cs
@@ -15,9 +15,9 @@ namespace TelerikAspNetCoreApp1.Controllers
 		{
             var result = Enumerable.Range(0, 50).Select(i => new OrderViewModel
             {
-                OrderID = i,
+                OrderID = (i+1),
                 Freight = i * 10,
-                OrderDate = new DateTime(2016, 9, 15).AddDays(i % 7),
+                OrderDate = new DateTime(2018, 2, 15).AddDays(i % 7),
 				ShipName = "ShipName " + i,
 				ShipCity = "ShipCity " + i
 			});
