93885fc89e17ef5f9d9f94a1ea14d1678eb6b2ab
david <shinvkvk@gmail.com>
2018-02-21 09:36:19 +0900

    브러치 머지

--- TelerikNodejsApp1/routes/index.js
+++ TelerikNodejsApp1/routes/index.js
@@ -11,6 +11,5 @@ exports.users = function (req, res) {
     var url = path.join(__dirname, '../views', 'users.html');
     console.log(" >>>>>>> users.url => " + url);
     res.sendFile(url);
-    //추후 수정브런치 머지할때 확인용
-    //res.sendFile('E:/Works/60.Project-Demo/samples-kendo/TelerikNodejsApp1/public/users.html');
+    //브런치에서 추가
 };
